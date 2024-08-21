91915addbc232aea0a52291fbcb865494fb81b79
oscar-wolfbrandt <oscar.schmererwolfbrandt@elev.ga.ntig.se>
2024-08-21 10:59:33 +0200

    klar

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 # Dice sum probability calculator
-# Författare: 
-# Datum:
+# Författare: oscar wolfbrandt
+# Datum:21/08/2024
 
+"""
 def main():
     user_input = input().split(" ")
 
 if __name__ == "__main__":
     main()
+"""
+import random  
+
+#takes input of dice 
+d1 = input("Sidor tärning 1:")
+d2 = input("sidor tärning 2:")
+list = []
+high = []
+pre = 0
+
+#makes list with all ressults 
+for x in range(1,int(d1)+1):
+    for y in range(1,int(d2)+1):
+        list.append(x+y)
+
+#counts the most common output 
+for p in range(1,int(d1)+int(d2)):
+    new = list.count(p)
+    if new > pre:
+        high.clear()
+        high.append(p)
+    elif new == pre:
+        high.append(p)
+    pre = list.count(p)
+
+
+
+print(high)
+
+
+
